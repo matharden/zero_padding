2118712f958135772ac1f1cdd3ad67782a3108ad
Mat Harden <matharden@gmail.com>
2013-10-04 22:38:54 +0100

    Switch assertion library from Should to Chai

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var
-  should = require('should'),
+  expect = require('chai').expect,
   zero_padding = require('../assets/zero_padding').zero_padding;
 
 describe('Zero padding', function(){
+  var result = zero_padding;
   it('should be a function');
   it('should accept one argument');
   it('should return a string');
@@ -10,36 +11,36 @@ describe('Zero padding', function(){
   describe('1 digit numbers', function(){
     var result = zero_padding(1);
     it('should be 3 digits long', function(){
-      result.should.have.lengthOf(3);
+      expect(result).to.have.length.of(3);
     });
     it('should have two leading zeros', function(){
-      result[0].should.equal('0');
-      result[1].should.equal('0');
-      result[2].should.not.equal('0');
+      expect(result[0]).to.equal('0');
+      expect(result[1]).to.equal('0');
+      expect(result[2]).not.to.equal('0');
     })
   })
 
   describe('2 digit numbers', function(){
     var result = zero_padding(21);
     it('should be 3 digits long', function(){
-      result.should.be.lengthOf(3);
+      expect(result).to.have.length.of(3);
     });
     it('should have one leading zero', function(){
-      result[0].should.equal('0');
-      result[1].should.not.equal('0');
-      result[2].should.not.equal('0');
+      expect(result[0]).to.equal('0');
+      expect(result[1]).not.to.equal('0');
+      expect(result[2]).not.to.equal('0');
     })
   })
 
   describe('3 digit numbers', function(){
     var result = zero_padding(999);
     it('should be 3 digits long', function(){
-      result.should.be.lengthOf(3);
+      expect(result).to.have.length.of(3);
     });
     it('should have no leading zeros', function(){
-      result[0].should.not.equal('0');
-      result[1].should.not.equal('0');
-      result[2].should.not.equal('0');
+      expect(result[0]).not.to.equal('0');
+      expect(result[1]).not.to.equal('0');
+      expect(result[2]).not.to.equal('0');
     })
   })
 })
